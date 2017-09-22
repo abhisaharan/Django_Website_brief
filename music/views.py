8aa4e0ee203771ba95d83b9c786695a0564bc939
abhisaharan <abhi.saharan@gmail.com>
2017-09-22 01:55:44 -0700

    model register with admin

--- music/views.py
+++ music/views.py
@@ -1,7 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import Album
 from django.shortcuts import HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is just beginning")
+    all_albums = Album.objects.all()
+    context = { 'all_albums': all_albums,}
+    return render(request, 'music/index.html', context)
 
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Uh oh! It happens. Album is in process of making")
+    return render(request, 'music/detail.html', {'album': album})
