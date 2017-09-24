1282c29676de36ee8edd8e101f682047553ad01f
abhisaharan <abhi.saharan@gmail.com>
2017-09-24 15:15:09 -0700

    login

--- music/urls.py
+++ music/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 app_name = 'music'  #we mentioned app_name here. So that in when html we will use 'music:detail'. It will not be confusing cause we will know 'detail' view belong to which app and also we can use 'detail' word for another view
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),    #as_view converts classes in views.py to view
+    url(r'^register/$', views.UserFromView.as_view(), name='register'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add/
+    url(r'^album/add/', views.AlbumCreate.as_view(), name='album-add'), # in tutorial it was like "r'^album/add/$'" but for me it worked without $ sign
+    url(r'^album/(?P<pk>[0-9]+)/', views.AlbumUpdate.as_view(), name='album-update'),
+    url(r'^album/(?P<pk>[0-9]+)/delete/', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,72 @@
 from django.views import generic
 from .models import Album
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 class IndexView(generic.ListView):
-    template_name =
+    template_name = "music/index.html"
+
+    def get_queryset(self):     #default name when you return a list object from ListView generic function is gonna be called object_list
+        return Album.objects.all()  #by default it return "object list". So even if it isnt mentioned here but you can see in index.html that we have used object_list
+
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = "music/detail.html"
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy("music:index")
+
+
+class UserFromView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    #register user
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #cleaned data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #return user objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
